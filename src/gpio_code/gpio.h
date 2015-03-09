b8eaca84f76edf9415587043a9f236eadc0fa726
Mike Lara <MikeLara@Mike-Laras-MacBook-Pro-2.local>
2015-03-08 19:24:46 -0700

    Updated GPIO code

--- src/gpio_code/gpio.h
+++ src/gpio_code/gpio.h
@@ -1,12 +1,12 @@
 /*
  * GPIO handler for Dragonboard
- * Author: Kenny Yokoyama
+ * Author: Kenny Yokoyama & Mike Lara
  */
 #ifndef __GPIO_H__
 #define __GPIO_H__
 
 #define GPIO_EXPORT_PATH  "/sys/class/gpio/export"
-#define GPIO_DIR_PATH     "/sys/class/gpio/gpio%d/direction"
+#define GPIO_MODE_PATH    "/sys/class/gpio/gpio%d/direction"
 #define GPIO_VALUE_PATH   "/sys/class/gpio/gpio%d/value"
 
 #define GPIO_HIGH 1
@@ -14,27 +14,46 @@
 #define GPIO_IN   "in"
 #define GPIO_OUT  "out"
 
+#define OUTPUT 1
+#define INPUT 0
+
 #define GPIO_NUM_COUNT  8
-#define GPIO_1  21
-#define GPIO_2  22
-#define GPIO_3  23
-#define GPIO_4  24
-#define GPIO_5  25
-#define GPIO_6  26
-#define GPIO_7  27
-#define GPIO_8  28
+#define GPIO_1  6					// 21
+#define GPIO_2  7					// 22
+#define GPIO_3  206					// 23
+#define GPIO_4  207					// 24
+#define GPIO_5  186					// 25
+#define GPIO_6  189					// 26
+#define GPIO_7  22					// 27
+#define GPIO_8  23					// 28
 
-#define P2I(x) (x-GPIO_1)
-
-class GPIO
+class GPIOPin
 {
   private:
-    int pinDesc[GPIO_NUM_COUNT];
-    int pinData[GPIO_NUM_COUNT];
+  	// Mike:
+  	int pinID;
+  	int linuxPinPID;
+  	int mode;
+  	int state;
+  	int fileID;
+  	//int pinMode[GPIO_NUM_COUNT];
+  	//int pinState[GPIO_NUM_COUNT];
+    
+    // Kenny:
+    //int pinDesc[GPIO_NUM_COUNT];
+    //int pinData[GPIO_NUM_COUNT];
 
   public:
-    void initPin(int pinID);
-    void setPin(int pinID, int state);
+  	// Kenny:
+    void initPin();
+    void setPin(int state);
+    // Mike:
+    int getPinState();
+    GPIOPin(int pinNumber);
+    ~GPIOPin();
+    void setPinMode(int pinMode);
+    int getPinMode();
+    void writeFile(int value);
 };
 
 #endif
