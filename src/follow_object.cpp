a1f8e74624e15e13bc8ffa0bbcfa95214f6e53b7
James Lee <batmanatucsd@gmail.com>
2015-03-11 22:58:29 -0700

    go forward works

--- src/follow_object.cpp
+++ src/follow_object.cpp
@@ -5,38 +5,53 @@
 class FollowObject {
 public:
   FollowObject();
-  Callback();
 
-private:
-  double linear_scale, angular_scale;
-  ros:NodeHandle handler;
+  ros::NodeHandle handler;
   ros::Publisher vel_pub;
   ros::Subscriber point_sub;
+
+private:
+  void Callback(const geometry_msgs::Point::ConstPtr&);
+  double linear_scale, angular_scale;
+  double goal_z;
 };
 
-FollowObject::FollowObject() {
+FollowObject::FollowObject() : goal_z(0.6), linear_scale(1.0), angular_scale(1.5) {
 
   handler.param("linear_scale", linear_scale, linear_scale);
   handler.param("angular_linear", angular_scale, angular_scale);
 
-  vel_pub = handler.advertise<geometry_msgs::Twist>("cmd_vel", 1, true);
-  point_sub = handler.subscribe<geometry_msgs::Point>("frank", 10,
+  vel_pub = handler.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity" , 1, true);
+  point_sub = handler.subscribe<geometry_msgs::Point>("/copter_center_3d", 10,
                   &FollowObject::Callback, this);
 
 }
 
-FollowObject::Callback(const geometry_msgs::Point::ConstPtr& point) {
+void FollowObject::Callback(const geometry_msgs::Point::ConstPtr& point) {
   geometry_msgs::Twist vel;
-  vel.angular.z = angular_scale*point->x;
-  vel.linear.x = linear_scale*point->z
+  if( point->x != 0 && point->z != 0) {
+    vel.angular.z = angular_scale*point->x;
+    vel.linear.x = linear_scale*(point->z - goal_z);
+  }
+  ROS_INFO("================ DEBUG =======================");
+  ROS_INFO("   Z = %lf, X = %lf", point->z, point->x);
   vel_pub.publish(vel);
 }
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "follower_object");
+  ros::init(argc, argv, "follow_object");
   FollowObject follow_object;
-  //ros::spin();
-  geometry_msgs::Twist vel;
-  vel.linear.x = linear_scale; 
-  vel_pub.publish(vel);
+  ros::spin();
+  
+  // DEBUG
+  //ros::NodeHandle handler;
+  //ros::Publisher vel_pub;
+  //vel_pub = handler.advertise<geometry_msgs::Twist>("cmd_vel", 1, true);
+
+  //while(ros::ok()) {
+    //geometry_msgs::Twist vel;
+    //vel.linear.x = 1; 
+    //follow_object.vel_pub.publish(vel);
+  //}
+  //DEBUG
 }
