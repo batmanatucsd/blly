a94ed0b754e0e9c809df414d9cc12a33eb802a31
James Lee <batmanatucsd@gmail.com>
2015-03-12 02:23:45 -0700

    find the depth average

--- src/follow_object.cpp
+++ src/follow_object.cpp
@@ -1,24 +1,34 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Point.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 class FollowObject {
 public:
-  ros::NodeHandle handler;
   ros::Publisher vel_pub;
-  ros::Subscriber point_sub;
   geometry_msgs::Twist vel;
   bool stop; 
 
   FollowObject();
 
 private:
-  void Callback(const geometry_msgs::Point::ConstPtr&);
+  void callback(const geometry_msgs::Point::ConstPtr&);
+  void callback_depth(const sensor_msgs::ImageConstPtr& msg);
+
   double linear_scale, angular_scale;
   double goal_z;
+
+  ros::NodeHandle handler;
+  image_transport::ImageTransport iTrans;
+  image_transport::Subscriber image_sub_depth;
+  ros::Subscriber point_sub;
 };
 
-FollowObject::FollowObject() : goal_z(0.5), linear_scale(0.25), angular_scale(1.5), stop(true) {
+FollowObject::FollowObject() : goal_z(0.5), linear_scale(0.25), angular_scale(1.5), stop(true), iTrans(handler) {
   handler.param("linear_scale", linear_scale, linear_scale);
   handler.param("angular_linear", angular_scale, angular_scale);
 
@@ -30,7 +40,7 @@ FollowObject::FollowObject() : goal_z(0.5), linear_scale(0.25), angular_scale(1.
   vel.angular.z = 0;
 }
 
-void FollowObject::Callback(const geometry_msgs::Point::ConstPtr& point) {
+void FollowObject::callback(const geometry_msgs::Point::ConstPtr& point) {
   if(point->x != 0 && point->y != 0 && point->z > 0) {
     vel.angular.z = -angular_scale*point->x;
     vel.linear.x = linear_scale*(point->z - goal_z);
@@ -46,24 +56,36 @@ void FollowObject::Callback(const geometry_msgs::Point::ConstPtr& point) {
   vel_pub.publish(vel);
 }
 
-double findContourDepth(const cv::Mat &contour_depth_F) {
-    double depth;
-    double min;
-    double max;
-    minMaxLoc(contour_depth_F, &min, &max);
-    cv::Mat contour_depth_U(contour_depth_F.rows, contour_depth_F.cols, CV_8UC1);
-    //ROS_INFO("\nmin masked: %lf\nmax masked: %lf", min, max);
-    contour_depth_F.convertTo(contour_depth_U, CV_8UC1, 255/RANGE_MAX, 0);
-    //imshow("contour with depth", contour_depth_U);
+void FollowObject::callback_depth(const sensor_msgs::ImageConstPtr& msg) {
+  cv_bridge::CvImagePtr depth_orig;
+   
+  try {
+      depth_orig = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_32FC1);
+  }
+  catch (cv_bridge::Exception& e) {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+  }
 
+  double average_depth = findAverageDepth(depth_orig->image);
+  
+  ROS_INFO("AVERAGE is DEPTH %lf", average_depth);
+
+  if(average_depth < 0.7) {
+    vel.linear.x = 0; 
+    vel_pub.publish(vel);
+  }
+}
+
+double findAverageDepth(const cv::Mat &depth_image_F) {
     float sum = 0;
     int count = 0;
-    for(int i = 0; i < contour_depth_F.rows; i++) {
-        for(int j = 0; j < contour_depth_F.cols; j++) {
 
-            const float curr = contour_depth_F.at<float>(i, j);
+    for(int i = 0; i < depth_image_F.rows; i++) {
+        for(int j = 0; j < depth_image_F.cols; j++) {
+
+            const float curr = depth_image_F.at<float>(i, j);
             if (curr == 0 || isnan(curr)) continue;
-            //ROS_INFO("curr: %lf", curr);
 
             sum += curr;
             count++;
